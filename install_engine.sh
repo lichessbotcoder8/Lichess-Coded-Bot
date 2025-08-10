dc05a52b4ac9585e3dbfba95105a0e5862def249
lichessbotcoder8 <kingvigneshultimate@gmail.com>
2025-08-10 21:22:59 +0530

    Update install_engine.sh

--- install_engine.sh
+++ install_engine.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
 set -e
-wget -O stockfish.tar https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-17.1-linux-x86-64.tar
+wget -O stockfish.tar https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-17.1-linux-x86-64-modern.tar
 tar -xf stockfish.tar
-mv stockfish-17.1-linux-x86-64/stockfish .
+mv stockfish-17.1-linux-x86-64-modern/stockfish .
 chmod +x ./stockfish
-rm -rf stockfish.tar stockfish-17.1-linux-x86-64
+rm -rf stockfish.tar stockfish-17.1-linux-x86-64-modern
+
 
