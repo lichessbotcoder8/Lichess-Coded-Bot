741b67ecb7fd3dbb942441c29c59d427f8c1cbf7
lichessbotcoder8 <kingvigneshultimate@gmail.com>
2025-08-10 21:18:48 +0530

    Update install_engine.sh

--- install_engine.sh
+++ install_engine.sh
@@ -1,4 +1,8 @@
-wget https://stockfishchess.org/files/stockfish-17.1-linux.zip
-unzip stockfish-17.1-linux.zip
-mv stockfish-17.1-linux/stockfish* ./stockfish
+#!/bin/bash
+set -e
+wget -O stockfish.tar https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-17.1-linux-x86-64.tar
+tar -xf stockfish.tar
+mv stockfish-17.1-linux-x86-64/stockfish .
 chmod +x ./stockfish
+rm -rf stockfish.tar stockfish-17.1-linux-x86-64
+
